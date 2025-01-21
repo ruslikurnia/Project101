ec6f4fbde76d2e480dd3e752f3aaebed159121ed
Ruslikurnia <ruslikurniawan313@gmail.com>
2025-01-21 10:19:16 +0700

    Website Project101

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//Ketika menu diklik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik diluar sidebar
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
